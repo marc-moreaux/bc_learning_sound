62b5e53d7513fdcdabb7a103e09d6952cbce6d2d
Marc <mr.moreaux@gmail.com>
2018-03-20 16:26:34 +0100

    train results/DB2_esc10_7c

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python main.py --dataset esc10 --netType envnet --BC --strongAugment --save results/esc10_6c --GAP --l1reg 1e-6 --data /media/moreaux/Data/Dataset/ENVNET_DB/
-python main.py --dataset esc10 --netType envnet --BC --strongAugment --save results/esc10_7c --GAP --l1reg 1e-7 --data /media/moreaux/Data/Dataset/ENVNET_DB/
-python main.py --dataset esc10 --netType envnet --BC --strongAugment --save results/esc10_8c --GAP --l1reg 1e-8 --data /media/moreaux/Data/Dataset/ENVNET_DB/
+# python main.py --dataset esc10 --netType envnet --BC --strongAugment --save results/DB2_esc10_6c --GAP --l1reg 1e-6 --data /media/moreaux/Data/Dataset/ENVNET_DB/
+python main.py --dataset esc10 --netType envnet --BC --strongAugment --save results/DB2_esc10_7c --GAP --l1reg 1e-7 --data /media/moreaux/Data/Dataset/ENVNET_DB/
+python main.py --dataset esc10 --netType envnet --BC --strongAugment --save results/DB2_esc10_8c --GAP --l1reg 1e-8 --data /media/moreaux/Data/Dataset/ENVNET_DB/
