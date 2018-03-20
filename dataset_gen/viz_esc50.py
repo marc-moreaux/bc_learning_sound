9cbdd2733a1746e4e578668feaf2482fcb5a7c00
Marc <mr.moreaux@gmail.com>
2018-03-20 16:23:52 +0100

    esc50.npz visualization

---
+++ dataset_gen/viz_esc50.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+
+DATASET_PATH = '/home/moreaux/Dataset/ENVNET_DB/esc10/wav16.npz'
+
+dataset = np.load(DATASET_PATH)
+sounds = dataset['fold1'].item()['sounds']
+lbls = dataset['fold1'].item()['labels']
+
+n_items = 5
+fig, axs = plt.subplots(n_items, n_items)
+for i in range(n_items * n_items):
+    ax = axs[i / n_items][i % n_items]
+    sound = sounds[i] / (25000.)
+    ax.plot(sound)
+    ax.set_ylim(-1, 1)
+    ax.set_title(lbls[i])
+    ax.set_axis_off()
+fig.show()
