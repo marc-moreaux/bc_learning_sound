63668257809a36e12bde5d0e010b5d77256b9766
Marc <mr.moreaux@gmail.com>
2018-09-14 18:40:05 +0200

    Roc curve as a function

--- test.py
+++ test.py
@@ -600,29 +600,13 @@ def get_cams(args):
     return xy_dict
 
 
-def roc_curve_from_cam():
-    pass
-
-
-def main2(args):
-    '''Evaluate localization
+def roc_curve_from_cam(xy_dict):
+    '''Given a cam, returns the ROC Curve
     '''
-    import utils; reload(utils)
-    import dataset; reload(dataset)
     from sklearn.metrics import roc_curve, auc
-    
-    xy_dict = get_cams(args)
-    import IPython; IPython.embed()
+    from scipy import interp
 
-    y = xy_dict['1-lbls2'].ravel()
-    pred = np.array([xy_dict['1-cams'][:,i].ravel() for i in range(50)])
-    fpr = dict()
-    tpr = dict()
-    roc_auc = dict()
-    for i in range(n_classes):
-        fpr[i], tpr[i], _ = roc_curve(y == i, pred[i])
-        roc_auc[i] = auc(fpr[i], tpr[i])
-
+    n_classes =  xy_dict['opt'].nClasses
     y_test = np.array([xy_dict['1-lbls2'].ravel() == i for i in range(n_classes)])
     y_score = np.array([xy_dict['1-cams'][:,i].ravel() for i in range(50)])
 
@@ -630,11 +614,68 @@ def main2(args):
     tpr = dict()
     roc_auc = dict()
     for i in range(n_classes):
-        fpr[i], tpr[i], _ = roc_curve(y_test[i], pred[i])
+        fpr[i], tpr[i], _ = roc_curve(y_test[i], y_score[i])
         roc_auc[i] = auc(fpr[i], tpr[i])
 
-    fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), pred.ravel())
+    fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel())
     roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+    # Compute macro-average ROC curve and ROC area
+
+    # First aggregate all false positive rates
+    all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+
+    # Then interpolate all ROC curves at this points
+    mean_tpr = np.zeros_like(all_fpr)
+    for i in range(n_classes):
+        mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+
+    # Finally average it and compute AUC
+    mean_tpr /= n_classes
+
+    fpr["macro"] = all_fpr
+    tpr["macro"] = mean_tpr
+    roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+    # Plot all ROC curves
+    plt.figure()
+    plt.plot(fpr["micro"], tpr["micro"],
+             label='micro-average ROC curve (area = {0:0.2f})'
+                   ''.format(roc_auc["micro"]),
+             color='deeppink', linestyle=':', linewidth=4)
+
+    plt.plot(fpr["macro"], tpr["macro"],
+             label='macro-average ROC curve (area = {0:0.2f})'
+                   ''.format(roc_auc["macro"]),
+             color='navy', linestyle=':', linewidth=4)
+
+    for i in range(n_classes):
+        plt.plot(fpr[i], tpr[i], lw=1,
+                 label='ROC curve of class {0} (area = {1:0.2f})'
+                 ''.format(i, roc_auc[i]))
+
+    plt.plot([0, 1], [0, 1], 'k--', lw=1)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Some extension of Receiver operating characteristic to multi-class')
+    plt.legend(loc="lower right")
+    plt.show()
+
+
+
+def main2(args):
+    '''Evaluate localization
+    '''
+    import utils; reload(utils)
+    import dataset; reload(dataset)
+    
+    xy_dict = get_cams(args)
+    
+    roc_curve_from_cam(xy_dict)
+
+
 
 
 if __name__ == '__main__':
